b9c8013432eb842f28a31654ac690003a85733ba
Lucien0907 <taowei.c@gmail.com>
2018-11-13 15:04:49 +0000

    success rate problem fixed

--- pattern_1_a.py
+++ pattern_1_a.py
@@ -10,51 +10,7 @@ from sklearn.model_selection import train_test_split
 
 import numpy as np
 import scipy.io as sio
-import matplotlib.pyplot as plt
-import matplotlib.ticker as ticker
-
-def compute_eigenspace(X_data, mode):
-    # Using high/low dimensional computation, return the average vector and the eigenspace of the given data.
-    N, D = X_data.shape
-    X_avg = X_data.mean(0)
-    X_avgm = np.array([X_avg]*N)
-    A = (X_data - X_avgm).T
-    if mode == "high":
-        S = A.dot(A.T) / N
-        e_vals, e_vecs = np.linalg.eig(S)
-    elif mode == "low":
-        S = (A.T).dot(A) / N
-        e_vals, e_vecs = np.linalg.eig(S)
-        e_vecs = np.dot(A,e_vecs)
-        e_vecs = e_vecs / np.linalg.norm(e_vecs, axis=0)
-    return X_avg, A, e_vals, e_vecs
-
-def plot_image(face_vector, w, h, filename):
-    # Reshape the given image data, plot the image
-    plt.figure()
-    image = np.reshape(np.absolute(face_vector),(w,h)).T
-    fig = plt.imshow(image, cmap = 'gist_gray')
-    fig.axes.get_xaxis().set_visible(False)
-    fig.axes.get_yaxis().set_visible(False)
-    plt.axis('off')
-    plt.savefig(filename, pad_inches = 0, bbox_inches='tight')
-    plt.close()
-    return
-
-def plot_graph(type, eig_value, i, x, y, xtick, ytick, filename):
-    # Plot the first i eigenvalues
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    if type == "bar":
-        plt.bar(list(range(0, i)), eig_value[:i])
-    else:plt.plot(list(range(0, i)), eig_value[:i])
-    ax.xaxis.set_major_locator(ticker.MultipleLocator(xtick))
-    ax.yaxis.set_major_locator(ticker.MultipleLocator(ytick))
-    plt.xlabel(x, fontsize=10)
-    plt.ylabel(y, fontsize=10)
-    plt.savefig(filename, pad_inches = 0, bbox_inches='tight')
-    #plt.show(block=False)
-    return
+import pr_functions as pr
 
 #Load image data
 mat_content = sio.loadmat('face.mat')
@@ -63,10 +19,10 @@ face_labels = mat_content['l']
 
 # Split into a training and testing set
 X_train, X_test, y_train, y_test = train_test_split(face_data.T, face_labels.T, test_size=0.3, random_state=42)
-X_avg, A, e_vals, e_vecs= compute_eigenspace(X_train, 'low')
+X_avg, A, e_vals, e_vecs= pr.compute_eigenspace(X_train, 'low')
 
 # Plot mean face
-plot_image(X_avg, 46, 56, 'Outputs/mean_face')
+pr.plot_image(X_avg, 46, 56, 'Outputs/mean_face')
 
 # Sort eigen vectors and eigen value in descending order
 idx=np.argsort(np.absolute(e_vals))[::-1]
@@ -75,9 +31,9 @@ e_vecs = (e_vecs.T[idx]).T
 
 # Choose and plot the best M eigenfaces
 M = 20
-plot_graph("bar", e_vals, M, 'index', 'eigen_value', 1, 100000, 'Outputs/first_'+str(M)+'_eigenvalues')
+pr.plot_graph("bar", e_vals, M, 'index', 'eigen_value', 1, 100000, 'Outputs/first_'+str(M)+'_eigenvalues')
 for i in range(M):
-    plot_image(e_vecs[:,i], 46, 56, 'Outputs/Eigenfaces/eigenface_'+str(i))
+    pr.plot_image(e_vecs[:,i], 46, 56, 'Outputs/Eigenfaces/eigenface_'+str(i))
 
 # Face reconstruction
 test1=29
@@ -86,8 +42,8 @@ Xa=e_vecs[:,:M]*X_proj
 X_reconst=X_avg+np.sum(Xa, axis=1)
 
 # Plot the chosen test face & reconstructed face for comparison
-plot_image(X_test[test1,:], 46, 56, 'Outputs/test_face_M='+str(M))
-plot_image(X_reconst, 46, 56, 'Outputs/reconstructed_face_M='+str(M))
+pr.plot_image(X_test[test1,:], 46, 56, 'Outputs/test_face_M='+str(M))
+pr.plot_image(X_reconst, 46, 56, 'Outputs/reconstructed_face_M='+str(M))
 
 # Classification
 M_range=30
@@ -106,6 +62,6 @@ for m in range(M_range):
     correctness[m] = (mark/test_size)*100
 
 # Plot success rate against M
-plot_graph("line", correctness, M_range, 'M', 'success rate', 5, 5, 'Outputs/success_rate_against_M')
+pr.plot_graph("line", correctness, M_range, 'M', 'success rate', 5, 5, 'Outputs/success_rate_against_M')
 print("Highest succes rate %.2f%% when M = %d" % (np.max(correctness), np.argmax(correctness)))
 #plt.show()
