97a6349e0f38a09610ae246639f50f5562788eab
Martinpanzy <36052046+Martinpanzy@users.noreply.github.com>
2018-11-17 22:53:53 +0000

    Add files via upload

--- pattern_1_c.py
+++ pattern_1_c.py
@@ -8,10 +8,14 @@ Created on Sun Nov 11 14:41:01 2018
 
 from __future__ import print_function
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
 
 import numpy as np
 import scipy.io as sio
 import pr_functions as pr
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 
 #Load image data
 mat_content = sio.loadmat('face.mat')
@@ -75,6 +79,8 @@ Wlda = e_vecs_lda[:,:Mlda]
 # Optimal fisherspace
 Wopt_t = np.dot(Wlda.T, Wpca.T)
 
+'''
+#pr.plot_graph("bar", Wopt_t, Mlda, 'index', 'eigen_value', 1, 100000, 'Outputs_c/eigenvlues')
 # Plot the best Mlda fisherfaces
 for i in range(Mlda):
     pr.plot_image(Wopt_t.T[:,i], 46, 56, 'Outputs_c/fisher_faces_'+str(i))
@@ -88,6 +94,7 @@ X_reconst=M+np.sum(Xa, axis=1)
 # Print the original face and reconstrcuted face for comparison
 pr.plot_image(X_test[test,:], 46, 56, 'Outputs_c/test_face_M='+str(Mlda))
 pr.plot_image(X_reconst, 46, 56, 'Outputs_c/reconstructed_face_M='+str(Mlda))
+
 
 # Classification using NN
 test_size = X_test.shape[0]
@@ -107,3 +114,65 @@ for m in range(Mlda):
     
 pr.plot_graph("line", correctness, Mlda, 'Mlda', 'success rate', 5, 5, 'Outputs_c/success_rate_against_M')
 print("Highest succes rate %.2f%% when M = %d" % (np.max(correctness), np.argmax(correctness)))
+
+
+#confusion matrix
+grade = 0
+test_size = X_test.shape[0]
+for i in range(test_size):
+    W_train = np.dot(X_train - M, Wopt_t.T)
+    W_test = np.dot(X_test[i,:] - M, Wopt_t.T)
+    E = np.linalg.norm(W_test - W_train, axis=1)
+    e_idx = np.argmin(E)
+    idx = y_train[e_idx]
+    if(i == 0):
+        y_pred = idx
+    else:
+        y_pred = np.hstack((y_pred, idx))
+    if(idx == y_test[i]):
+        grade = grade + 1
+cnf_matrix = confusion_matrix(y_test, y_pred)
+
+sns.heatmap(cnf_matrix, cmap="Blues", xticklabels = 5, yticklabels = 5)
+sns.plt.show()
+grade = grade/test_size
+
+'''
+
+
+#heatmap 
+Mpca = 3
+Mlda = 2
+recog_acc_mt = np.zeros((Mlda, Mpca))
+test_size = X_test.shape[0]
+for p in range(Mpca):
+    Wpca = e_vecs_pca[:,:(p*10)]
+    for l in range(Mlda):
+        mark = 0
+        # Compute eigen space Wlda
+        SB = np.dot(np.dot(Wpca.T, Sb), Wpca)
+        SW = np.dot(np.dot(Wpca.T, Sw), Wpca)
+        e_vals_lda, e_vecs_lda = np.linalg.eig(np.dot(np.linalg.inv(SW), SB))
+        # Sort and choose the best Mlda eigenvectors
+        idx1=np.argsort(np.absolute(e_vals_lda))[::-1]
+        e_vals_lda = e_vals_lda[idx1]
+        e_vecs_lda = (e_vecs_lda.T[idx1]).T
+        Wlda = e_vecs_lda[:,:(l*2)]
+        # Optimal fisherspace
+        Wopt_t = np.dot(Wlda.T, Wpca.T)
+        for i in range(test_size):
+           W_train = np.dot(X_train - M, Wopt_t.T)
+           W_test = np.dot(X_test[i,:] - M, Wopt_t.T)
+           E = np.linalg.norm(W_test - W_train, axis=1)
+           e_idx = np.argmin(E)
+           if y_train[e_idx] == y_test[i]:
+               mark+=1
+        recog_acc_mt[l,p] = (mark/test_size)*100
+
+ax = sns.heatmap(recog_acc_mt, cmap="Blues", square = True, xticklabels = 5, yticklabels = 5)
+
+ax.axhline(y=0, color='k',linewidth=5)
+ax.axvline(x=0, color='k',linewidth=5)
+ax.axhline(y=Mlda, color='k',linewidth=5)
+ax.axvline(x=Mpca, color='k',linewidth=5)
+plt.show()
